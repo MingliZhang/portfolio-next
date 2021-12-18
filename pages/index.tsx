b1f3aa7d6912b10bf45be40914a3bcf9cf6c9559
MingliZhang <31600683+MingliZhang@users.noreply.github.com>
2021-12-18 12:00:26 -0500

    update

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,23 +11,9 @@ const index = ({ services }) => {
 
 export default index;
 
-// export const getServerSideProps = async (context: GetServerSideProps) => {
-//     const res = await fetch("http://localhost:3000/api/services");
-//     const data = await res.json();
-
-//     console.log("Server:", data.services);
-
-//     return {
-//         props: {
-//             services: data.services,
-//         },
-//     };
-// };
 export const getStaticProps = async (context: GetStaticProps) => {
     const res = await fetch("http://localhost:3000/api/services");
     const data = await res.json();
-
-    console.log("Server:", data.services);
 
     return {
         props: {
