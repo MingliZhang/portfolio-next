e81d38bdb456738ae5e5bc23bb5b5751750f40d8
MingliZhang <31600683+MingliZhang@users.noreply.github.com>
2021-12-18 12:16:52 -0500

    removed server side props

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,23 +1,22 @@
 import { GetServerSideProps, GetStaticProps } from "next";
 // import { services } from "../data";
-const index = ({ services }) => {
-    console.log("local:", services);
+const index = () => {
     return (
         <div>
-            <h1>Bye world</h1>
+            <h5></h5>
         </div>
     );
 };
 
 export default index;
 
-export const getStaticProps = async (context: GetStaticProps) => {
-    const res = await fetch("http://localhost:3000/api/services");
-    const data = await res.json();
+// export const getStaticProps = async (context: GetStaticProps) => {
+//     const res = await fetch("http://localhost:3000/api/services");
+//     const data = await res.json();
 
-    return {
-        props: {
-            services: data.services,
-        },
-    };
-};
+//     return {
+//         props: {
+//             services: data.services,
+//         },
+//     };
+// };
