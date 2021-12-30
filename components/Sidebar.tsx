d702f5d80a1a3d995a5cd71b7f155b6b3a346d59
MingliZhang <31600683+MingliZhang@users.noreply.github.com>
2021-12-29 20:26:59 -0500

    small change to words

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -42,7 +42,7 @@ const Sidebar = () => {
                 target="_blank."
             >
                 <GiTie className="w-6 h-6 " />
-                Old Website
+                Old Portfolio
             </a>
             <a
                 className="flex items-center justify-center px-2 py-1 my-3 bg-gray-200 rounded-full dark:bg-dark-200"
