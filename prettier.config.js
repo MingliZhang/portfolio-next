364a1fe1c607f91d8b90578eab31577c05fb596a
MingliZhang <31600683+MingliZhang@users.noreply.github.com>
2022-02-13 22:01:21 -0500

    updated prettier config

---
+++ prettier.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    trailingComma: "es5",
+    tabWidth: 2,
+    useTabs: true,
+    semi: true,
+    singleQuote: true,
+    bracketSpacing: true,
+    bracketSameLine: true,
+    arrowParens: "always",
+};
