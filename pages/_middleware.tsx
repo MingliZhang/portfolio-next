910c0c5180b7cd643360a5a20ea799f95c14d21a
MingliZhang <31600683+MingliZhang@users.noreply.github.com>
2022-02-15 16:35:04 -0500

    update test

--- pages/_middleware.tsx
+++ pages/_middleware.tsx
@@ -1,7 +1,6 @@
-import redirects from '@lib/redirects';
 import { NextFetchEvent, NextRequest, NextResponse } from 'next/server';
 
-export async function middleware(req: NextRequest, ev: NextFetchEvent) {
+export function middleware(req: NextRequest, ev: NextFetchEvent) {
 	return NextResponse.redirect(
 		`https://${req.nextUrl.hostname}${req.nextUrl.pathname}`,
 		307
