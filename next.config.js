6a86128514fa024e0fc1df43a9437854ced1c364
Mike <zml2416154671@gmail.com>
2023-07-13 15:57:02 +0800

    deploy

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 module.exports = {
-	reactStrictMode: true,
-	images: {
-		domains: ['minglizhang.com'],
-	},
+    reactStrictMode: true,
+    images: {
+        domains: ['minglizhang.com'],
+        unoptimized: true,
+    },
 };
